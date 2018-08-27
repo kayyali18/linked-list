adc7319c08dba096a75e8379ff2e26142a5c54f0
Ahmad Kayyali <kayyali18@gmail.com>
2018-08-27 11:25:33 -0600

    Change origin to allow for commits

--- scripts/LinkedList.js
+++ scripts/LinkedList.js
@@ -1,1 +1,64 @@
-export default class LinkedList {}
+import Node from './Node';
+
+export default class LinkedList {
+    constructor () {
+        this.length = 0;
+        this.head = null;
+    }
+
+    push (data) {
+        this.length++;
+
+        if (!this.head) this.head = new Node (data);
+        else {
+            let currNode = this.head;
+
+            while (currNode.next) {
+                currNode = currNode.next;
+            }
+
+            currNode.next = new Node(data);
+        }
+    }
+
+    // //My build
+    // pop () {
+    //     if (!this.head) return null;
+    //     else if (this.length == 0) {
+    //         return;
+    //     } else if (this.length == 1) {
+    //         this.length--;
+    //         let node = this.head;
+    //         this.head = null;
+    //         return node;
+    //     } else {
+    //         this.length--;
+    //         let currNode = this.head;
+
+    //         for (let i = 0; i < this.length - 1; i++) {
+    //             currNode = currNode.next;
+    //         }
+
+    //         let poppedOff = currNode.next;
+    //         currNode.next = null;
+    //         return poppedOff;
+    //     }
+    // }
+
+    // Nathan's Build
+    pop () {
+        let currNode = this.head;
+        let prevNode;
+
+        if (!this.head) return null;
+        while (currNode.next) {
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        if (this.length == 1) this.head = null;
+        if (prevNode) prevNode.next = null;
+        this.length--;
+        return currNode;
+
+    }
+}
